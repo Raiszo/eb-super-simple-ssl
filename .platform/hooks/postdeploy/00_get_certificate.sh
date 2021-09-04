0ae341805f935f76d8ef6353a883ef878e167b36
Raiszo <E05.sek@gmail.com>
2021-09-03 22:35:14 -0500

    feat: gonna use a env-var when calling certbot

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # remember to replace DOMAIN and EMAIL
-sudo certbot -n -d testing-eb.prestamype.co --nginx --agree-tos --email echoquehuanca@prestamype.com
+sudo certbot -n -d $DOMAIN_FOR_CERTBOT --nginx --agree-tos --email echoquehuanca@prestamype.com
